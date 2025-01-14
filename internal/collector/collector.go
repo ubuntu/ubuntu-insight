49a3331858403b411c53081f35f0d0b2bb6c0b47
kat <28567881+hk21702@users.noreply.github.com>
2025-01-14 14:36:16 -0500

    Add package comments

--- internal/collector/collector.go
+++ internal/collector/collector.go
@@ -1,1 +1,4 @@
+// Package collector provides the implementation of the collector component.
+// The collector is responsible for collecting the metrics from the different sources, compiling them into a single report,
+// and sending them to persistent storage.
 package collector
--- internal/uploader/uploader.go
+++ internal/uploader/uploader.go
@@ -1,1 +1,3 @@
+// Package uploader provides the implementation of the uploader component.
+// The uploader is responsible for uploading the report created by the collector to the remote server.
 package uploader
